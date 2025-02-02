e2e5a725215c2c23b70361c321fee208a7807575
SumeetDarje <114173649+SumeetDarje@users.noreply.github.com>
2025-02-02 19:40:50 +0530

    Add From to be completed

--- React/Assignment66/src/Assi66.jsx
+++ React/Assignment66/src/Assi66.jsx
@@ -6,6 +6,7 @@ import LoginPage from "./LoginPage";
 import AdminPage from "./AdminPage";
 import axios from "axios";
 import { BeatLoader } from "react-spinners";
+import AdminProductFormSample from "./AdminProductForm";
 
 function Assi66() {
   // let pList = [
@@ -198,7 +199,6 @@ function Assi66() {
   //   },
   // ];
 
-  // let p=[...productList]
 
   let [productList, setProductList] = useState([]);
   let [filteredProductList, setFilteredProductList] = useState([]);
@@ -207,13 +207,22 @@ function Assi66() {
   let [cartCount, setCartCount] = useState(0);
 
   let [message, setMessage] = useState("");
-  // let [adminProduct, setAdminProduct] = useState("");
+  let [adminView, setAdminView] = useState("");
+  let [selectedProduct, setSelectedProduct] = useState("");
   let [loadFlag, setLoadFlag] = useState(false);
 
   // let [userView, setUserView] = useState("admin");
-  let [userView, setUserView] = useState("products");
+  // let [userView, setUserView] = useState("products");
+  let [userView, setUserView] = useState("loader");
 
-  let [userName,setUserName]=useState("")
+  let [userName, setUserName] = useState("");
+
+  // useEffect(()=>{
+  //   let loader=setTimeout(()=>{
+  //     setLoadFlag(true);
+  //   },1500);
+  //   return ()=> clearTimeout(loader);
+  // },[])
 
   function handleAddToCartBtn(selectedIndex, action) {
     console.log(selectedIndex, action);
@@ -235,7 +244,7 @@ function Assi66() {
 
     let fprList = filteredProductList.map((e, index) => {
       if (index == selectedIndex) {
-        if (action == "add") {
+        if (action == "addToCart") {
           e.qty = 1;
           setCartCount(cartCount + 1);
         } else if (action == "+") {
@@ -279,9 +288,13 @@ function Assi66() {
   async function getData() {
     console.log("got the data");
     setLoadFlag(true);
+    setTimeout(() => {
+      setLoadFlag(false);
+      setUserView("products");
+    }, 1500);
     let response = await axios("http://localhost:3000/fruits");
     let fList = await response.data;
-    setLoadFlag(false);
+    // setLoadFlag(false);
     console.log("Loaded " + fList.length);
     setProductList(fList);
     setFilteredProductList(fList);
@@ -291,11 +304,11 @@ function Assi66() {
     setUserView("signupPage");
   }
 
-  function handleLoginClick(){
-    setUserView("loginPage")
+  function handleLoginClick() {
+    setUserView("loginPage");
   }
-  
-  function handleBackBtn(){
+
+  function handleBackBtn() {
     setUserView("products");
   }
 
@@ -311,49 +324,67 @@ function Assi66() {
     // if(status == "admin"){
     //   setUserView("admin");
     //   console.log("Admin Hello");
-      
+
     // }
-
   }
 
-  function handleAdminLogin(user){
+  function handleAdminLogin(user) {
     setUserView("admin");
-    console.log(user.name+" Admin ahe");
-    setUserName(user.name);
-  }
-  
-  function handleUserLogin(user){
-    setUserView("user");
-    console.log(user.name+" User ahe me");    
+    console.log(user.name + " Admin ahe");
     setUserName(user.name);
   }
 
-  function handleLogout(){
+  function handleUserLogin(user) {
+    setUserView("user");
+    console.log(user.name + " User ahe me");
+    setUserName(user.name);
+  }
+
+  function handleLogout() {
     setUserView("products");
     console.log("logout");
-    
   }
 
-  function handleEditButton(card){
-    console.log("Edit "+card);
-    
+  function handleEditButton(card) {
+    console.log("Editing " + card.name);
+    setAdminView("edit");
+    setSelectedProduct(card);
+    setUserView("form");
   }
-  function handleDeleteButton(card){
+
+  function handleFormSubmit(updatedProduct) {
+    if (adminView == "edit") {
+      let updatedList = productList.map((p) =>
+        p.id == updatedProduct.id ? updatedProduct : p
+      );
+      console.log("Edit Form Submit for " + updatedProduct.name);
+
+      setFilteredProductList(updatedList);
+      setProductList(updatedList);
+    }
+    setUserView("admin");
+  }
+
+  function handleProductListClick() {
+    console.log("Product List Click ");
+    setUserView("admin");
+  }
+
+  function handleDeleteButton(card) {
     // console.log("Delete "+card.id);
 
-    
-      let eleDelete=filteredProductList.filter((product)=> product.id != card.id );
-      console.log(`Delete ${card.name}`);
+    let eleDelete = filteredProductList.filter(
+      (product) => product.id != card.id
+    );
+    console.log(`Delete ${card.name}`);
 
-      setFilteredProductList(eleDelete);
-      setProductList(eleDelete);
+    setFilteredProductList(eleDelete);
+    setProductList(eleDelete);
 
-      setMessage(`${card.name} fruit deleted successfully!`)
+    setMessage(`${card.name} fruit deleted successfully!`);
 
-      setTimeout(()=>setMessage(""),4000); 
+    setTimeout(() => setMessage(""), 4000);
   }
-
-  
 
   return (
     <>
@@ -367,46 +398,54 @@ function Assi66() {
         userView={userView}
         onlogout={handleLogout}
         userName={userName}
-
       />
       <div className="container containerMargin">
+        <div className="text-center">
+          {loadFlag && <BeatLoader size={24} color={"red"} />}
+        </div>
 
-      <div className="text-center">
-        {loadFlag && <BeatLoader size={24} color={"red"} />}
-      </div>
-      {userView == "signupPage" && <SignupPage 
-        onLoginButtonClick={handleLoginClick}
-        onBackBtn={handleBackBtn}
-        // userView={userView}
-        />}
+        {userView == "signupPage" && (
+          <SignupPage
+            onLoginButtonClick={handleLoginClick}
+            onBackBtn={handleBackBtn}
+            // userView={userView}
+          />
+        )}
 
-      {userView == "loginPage" && (
-        <LoginPage 
-        onLoginSubmit={handleLoginSubmit} 
-        onAdminLogin={handleAdminLogin}
-        onUserLogin={handleUserLogin}
-        onBackBtn={handleBackBtn}
-        />
-      )}
+        {userView == "loginPage" && (
+          <LoginPage
+            onLoginSubmit={handleLoginSubmit}
+            onAdminLogin={handleAdminLogin}
+            onUserLogin={handleUserLogin}
+            onBackBtn={handleBackBtn}
+          />
+        )}
 
-      {userView == "admin"  && (
-        
+        {userView == "admin" && (
           <div className=" row mt-5 ">
-          {filteredProductList.map((e, i) => (
-              <AdminPage  
-               onEditBtn={handleEditButton}
-               onDeleteBtn={handleDeleteButton}
-              card={e}
-              key={i}
-              index={i}
-                
+            {filteredProductList.map((e, i) => (
+              <AdminPage
+                onEditBtn={handleEditButton}
+                onDeleteBtn={handleDeleteButton}
+                card={e}
+                key={i}
+                index={i}
               />
             ))}
           </div>
-      )}
+        )}
 
-      {userView == "products"  && (
-        
+        {userView == "form" && (
+          <AdminProductFormSample
+            adminView={adminView}
+            product={selectedProduct}
+            onProductEditFormSubmit={handleFormSubmit}
+            onProductAddFormSubmit={handleFormSubmit}
+            onProductListClick={handleProductListClick}
+          />
+        )}
+
+        {userView == "products" && (
           <div className=" row mt-5 ">
             {filteredProductList.map((e, i) => (
               <CardList
@@ -419,26 +458,22 @@ function Assi66() {
               />
             ))}
           </div>
-        
-      )}
-
-      {userView == "user"  && (
-        
-        <div className=" row mt-5 ">
-          {filteredProductList.map((e, i) => (
-            <CardList
-              onClickBtn={handleAddToCartBtn}
-              totalPrice={e.mrp * e.qty}
-              card={e}
-              key={i}
-              index={i}
-              userView={userView}
-            />
-          ))}
-        </div>
-      
         )}
 
+        {userView == "user" && (
+          <div className=" row mt-5 ">
+            {filteredProductList.map((e, i) => (
+              <CardList
+                onClickBtn={handleAddToCartBtn}
+                totalPrice={e.mrp * e.qty}
+                card={e}
+                key={i}
+                index={i}
+                userView={userView}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
