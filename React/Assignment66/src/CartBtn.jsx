a2c6cf034022be23e71f6183fcb9b59808aeb10b
SumeetDarje <114173649+SumeetDarje@users.noreply.github.com>
2025-03-19 16:39:02 +0530

    need to update

--- React/Assignment66/src/CartBtn.jsx
+++ React/Assignment66/src/CartBtn.jsx
@@ -1,16 +1,73 @@
 import React from "react";
+import LoginPage from "./LoginPage";
 
 function CartBtn(props) {
+  let { userName } = props;
+  let { cartCount } = props;
 
-    function handleCartListClick() {
-        props.onCartListClick();
-    }
+  function handleCartListClick() {
+    props.onCartListClick();
+  }
 
-return (
-    <div className="text-center">
-      Cart is Empty. <a role="button" className="text-primary" onClick={handleCartListClick}>Start</a> Shopping
-    </div>
-)
+  function handleCartBack(){
+    props.onCartBack();
+  }
+
+  return (
+    <>
+      {userName == "" && cartCount == 0 && (
+        <div className="text-center">
+          Cart is Empty.{" "}
+          <a
+            role="button"
+            className="text-primary"
+            onClick={handleCartListClick}
+          >
+            Start
+          </a>{" "}
+          Shopping
+        </div>
+      )}
+
+      {userName != "" && cartCount == 0 && (
+        <div className="text-center">
+          Welcome, {userName}! <br />
+          Cart is Empty.{" "}
+          <a
+            role="button"
+            className="text-primary"
+            onClick={handleCartListClick}
+          >
+            Start
+          </a>{" "}
+          Shopping
+        </div>
+      )}
+
+      {userName != "" && cartCount > 0 && (
+        <div className="text-center justify-content-center align-items-center ">
+          <div className="">
+            <a role="button" className="text-primary" onClick={handleCartBack}>
+              Back
+            </a>{" "}
+            to Shopping.
+          </div>
+          
+          <div className="mt-3 mb-1">
+          <a role="button" className="text-primary" onClick={handleCartBack}>
+              Proceed
+            </a>{" "}
+            to Buy.
+          </div>
+
+          <div className="mt-2 cartBorder w-50 m-auto">
+            hello
+          </div>
+        </div>
+
+      )}
+    </>
+  );
 }
 
-export default CartBtn
+export default CartBtn;
