be3498d4fbcd871355ac58aa0fc58bee4747e45f
SumeetDarje <114173649+SumeetDarje@users.noreply.github.com>
2025-03-20 21:01:05 +0530

    ok

--- React/Assignment66/src/CardList.jsx
+++ React/Assignment66/src/CardList.jsx
@@ -1,24 +1,23 @@
 function CardList(props) {
   let { card } = props;
   let { index } = props;
-  let {userView} = props;
+  let { userView } = props;
 
   function handleAddToCartBtn() {
     // console.log(card.inStock);
 
-    props.onClickBtn(index,"addToCart");
+    props.onClickBtn(index, "addToCart");
   }
 
-  function handleBtnClick(action){
+  function handleBtnClick(action) {
     // console.log(action);
-    props.onClickBtn(index,action);
-    
+    props.onClickBtn(index, action);
   }
 
   // let totalPrice=card.qty*card.mrp;
 
   return (
-    <> 
+    <>
       <div className=" col-lg-3 col-sm-12 col-md-6  text-center p-3 mb-4 myBorder">
         <div>
           <img className="img-fluid" src={`/images/${card.image}`} alt="" />
@@ -27,18 +26,24 @@ function CardList(props) {
           <div className="discount"> {card.discount} %Discount </div>
         )}
         <div>{card.name}</div>
-        
-        <div className="mt-2">Rs. 
-          <span className="text-decoration-line-through">{card.discount==0 ? "": 
-          // (card.mrp-(card.mrp*card.discount)/100)} 
-        (card.mrp)} 
-          </span>  {" "}
-          {(card.mrp-(card.mrp*card.discount)/100)}
+
+        <div className="mt-2">
+          Rs.
+          <span className="text-decoration-line-through">
+            {card.discount == 0
+              ? ""
+              : // (card.mrp-(card.mrp*card.discount)/100)}
+                card.mrp}
+          </span>{" "}
+          {card.mrp - (card.mrp * card.discount) / 100}
         </div>
 
         {card.qty == 0 && card.inStock == true && (
           <div>
-            <button className="btn btn-danger w-100" onClick={handleAddToCartBtn}>
+            <button
+              className="btn btn-danger w-100"
+              onClick={handleAddToCartBtn}
+            >
               {" "}
               Add To Cart
             </button>
@@ -46,10 +51,7 @@ function CardList(props) {
         )}
         {!card.inStock && (
           <div>
-            <button className="btn btn-secondary w-100" >
-              {" "}
-              Out of Stock
-            </button>
+            <button className="btn btn-secondary w-100"> Out of Stock</button>
           </div>
         )}
 
@@ -74,9 +76,7 @@ function CardList(props) {
             </div>
           </div>
         )}
-        {card.qty!=0 && (
-          <div className="">Rs.{props.totalPrice}</div>
-        )}
+        {card.qty != 0 && <div className="">Rs.{props.totalPrice}</div>}
       </div>
     </>
   );
