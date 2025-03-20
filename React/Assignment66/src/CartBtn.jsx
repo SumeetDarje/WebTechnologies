891ebbc83f40984ad8560667b439969fd1df5c7e
SumeetDarje <114173649+SumeetDarje@users.noreply.github.com>
2025-03-20 20:59:50 +0530

    ok

--- React/Assignment66/src/CartBtn.jsx
+++ React/Assignment66/src/CartBtn.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import LoginPage from "./LoginPage";
 
-function CartBtn(props) {
+function CartPage(props) {
   let { userName } = props;
   let { cartCount } = props;
+  let { cartItems } = props;
 
   function handleCartListClick() {
     props.onCartListClick();
   }
 
-  function handleCartBack(){
+  function handleCartBack() {
     props.onCartBack();
+  }
+
+  function handleCartBtnClick(action , itemIndex) {
+    // console.log("Action: ",action+" index: ",itemIndex);
+    
+    props.onCartAddRemoveProduct( itemIndex ,action );
   }
 
   return (
@@ -52,22 +58,53 @@ function CartBtn(props) {
             </a>{" "}
             to Shopping.
           </div>
-          
+
           <div className="mt-3 mb-1">
-          <a role="button" className="text-primary" onClick={handleCartBack}>
+            <a role="button" className="text-primary" onClick={handleCartBack}>
               Proceed
             </a>{" "}
             to Buy.
           </div>
 
-          <div className="mt-2 cartBorder w-50 m-auto">
-            hello
+          <div className="mt-2 w-50 m-auto">
+            {cartItems.length > 0 &&
+              cartItems.map((item, index) => (
+                <div key={index} className=" row mt-2 cartBorder">
+                
+                  <div className="col-1 p-2 ">{index + 1}.</div>
+                  <div className="col-3 p-2 text-start textBold ">
+                    {item.name}
+                  </div>
+                  <div className="col-8 p-2 text-end ">Rs. {item.mrp}</div>
+
+                  <div className="row p-2">
+                    <div className="col-2">
+                      <button
+                        className="btn btn-danger btn-block"
+                        onClick={() => handleCartBtnClick("-",index)}
+                      >
+                        -
+                      </button>{" "}
+                    </div>
+                    <div className="col-2">Qnty: {item.qty}</div>
+                    <div className="col-2">
+                      <button
+                        className="btn btn-danger "
+                        onClick={() => handleCartBtnClick("+",index)}
+                      >
+                        +
+                      </button>{" "}
+                    </div>
+                    <div className="col-6 text-end">Total Rs.{ item.mrp * item.qty }</div>
+                  </div>
+
+                </div>
+              ))}
           </div>
         </div>
-
       )}
     </>
   );
 }
 
-export default CartBtn;
+export default CartPage;
